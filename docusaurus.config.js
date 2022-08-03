d1dc0f3d2d95f444ac328ec55f5e32ede8fb9e1f
pierre <pierre.malherbe@360learning.com>
2022-08-03 20:44:28 +0200

    feat(chore): Configure DNS + Website info
    * Change base URL
    * Configure some confs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Pierre Malherbe',
+  tagline: 'Pierre Malherbe Website, courses and blog',
+  url: 'https://pierremalherbe.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,15 +16,15 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Pierre-Malherbe', // Usually your GitHub org/user name.
+  projectName: 'website', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'fr',
+    locales: ['fr'],
   },
 
   presets: [
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Pierre Malherbe',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
